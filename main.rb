61c96bd185b6ac6c592e06f396f1585464a4feac
Robert Ness <robertness@gmail.com>
2014-03-03 22:37:36 -0500

    changed name to asset

--- main.rb
+++ main.rb
@@ -2,13 +2,13 @@ require 'sinatra'
 require 'slim'
 require 'data_mapper'
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/development.db")
-	class Post
+	class Asset
 		include DataMapper::Resource
 		property :id,			 Serial, :key => true
 		property :name, 		 String, :required => true
 		property :rent,			 Integer, :required => true
-		property :automatable?,	 Bool, :default => false
-		property :controllable?, Bool, :default => false
+		property :automatable?,	 Boolean, :default => false
+		property :controllable?, Boolean, :default => false
 	end
 
 
