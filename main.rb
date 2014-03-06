cb9d94ded8193757ab843a00fee2e5fd343fc935
simpsonness <corallishan@gmail.com>
2014-03-06 13:51:36 +0700

    add name in database, working

--- main.rb
+++ main.rb
@@ -1,35 +1,25 @@
 require 'sinatra'
-require 'erb'
-require 'data_mapper'
-require 'sinatra'
 require 'slim'
 require 'data_mapper'
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/development.db")
-	class Asset
-		include DataMapper::Resource
-		property :id,			 Serial, :key => true
-		property :name, 		 String, :required => true
-		property :rent,			 Integer, :required => true
-		property :automatable?,	 Boolean, :default => false
-		property :controllable?, Boolean, :default => false
-	end
 
-
+class Asset
+	include DataMapper::Resource
+	property :id,             Serial
+	property :name,           String, :required => true
+end
+DataMapper.finalize
 
 get '/' do
-  slim :index
+	@assets = Asset.all
+	slim :index
 end
- 
-get '/:item' do
-	@item = params[:item]
-	slim :item
+
+get '/asset' do
+	slim :asset
 end
 
 post '/' do
-	@item = params[:item]
-	slim :item
+	Asset.create params[:asset]
+	redirect to('/')
 end
-
-
-
-
